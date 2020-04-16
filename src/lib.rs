6710bd0d1437c9e5563f531215908661c4d57070
Amos Wenger <amoswenger@gmail.com>
2020-04-17 00:03:33 +0200

    clean up windows hooking code

--- src/lib.rs
+++ src/lib.rs
@@ -1,96 +1,18 @@
 use nj_sys as sys;
 use std::{ffi::CString, ptr};
 
+#[cfg(windows)]
+mod winhook;
+
 macro_rules! fixup {
     ($name: expr, ($($t:ty),*)) => {{
         #[cfg(windows)]
         unsafe {
-            const PAGE_EXECUTE_READWRITE: u32 = 0x40;
-            use winapi::ctypes::c_void;
-
-            let f: unsafe extern "C" fn($($t),*) -> _ = $name;
-            let real = lookup(f, stringify!($name));
-
-            println!("name = {:?}, real = {:?}", $name as *const (), real as *const ());
-            let diff: isize = $name as isize - real as isize;
-            println!("diff = {:?}, i32::max = {:?}", diff, std::i32::MAX);
-
-            // TODO: support 32-bit
-            let mut template: Vec<u8> = vec![0x48, 0xb8];
-            template.extend(&(real as usize).to_le_bytes());
-            template.extend(&[0xff, 0xe0]);
-            print!("full template is {} bytes:", template.len());
-            for b in &template {
-                print!(" {:02x}", b);
-            }
-            println!();
-
-            let mut old_prot = 0;
-            let new_prot = PAGE_EXECUTE_READWRITE;
-            println!("de-protecting...");
-            let ret = winapi::um::memoryapi::VirtualProtect(
-                $name as *mut c_void,
-                template.len(),
-                new_prot,
-                &mut old_prot,
-            );
-            println!("ret = {:?}", ret);
-            if (ret == 0) {
-                panic!("could not de-protect");
-            }
-
-            println!("copying...");
-            std::ptr::copy_nonoverlapping(
-                template.as_ptr(),
-                $name as *mut u8,
-                template.len(),
-            );
-
-            println!("re-protecting...");
-            let ret = winapi::um::memoryapi::VirtualProtect(
-                $name as *mut c_void,
-                template.len(),
-                old_prot,
-                &mut old_prot,
-            );
-            println!("ret = {:?}", ret);
-            if (ret == 0) {
-                panic!("could not re-protect");
-            }
-
-            // println!("detouring {:?} => {:?}", $name as *const (), real as *const ());
-            // let detour = detour::RawDetour::new(
-            //     $name as *const (),
-            //     real as *const (),
-            // ).unwrap();
-            // detour.enable().unwrap();
-            // Box::leak(Box::new(detour));
+            let thunk: unsafe extern "C" fn($($t),*) -> _ = $name;
+            winhook::hook(stringify!($name), thunk as *const std::ffi::c_void);
         }
     }};
 }
-
-#[cfg(windows)]
-fn lookup<T>(_t: T, name: &str) -> T {
-    let name = name.split("::").last().unwrap();
-    let name = CString::new(name).unwrap();
-    let addr = unsafe { winapi::um::libloaderapi::GetProcAddress(ptr::null_mut(), name.as_ptr()) };
-    if addr.is_null() {
-        panic!("could not find {:?}", name);
-    }
-    println!("looked up {:?} => {:?}", name, addr);
-    unsafe { std::mem::transmute_copy(&addr) }
-}
-
-#[used]
-#[cfg_attr(target_os = "linux", link_section = ".ctors")]
-#[cfg_attr(target_os = "macos", link_section = "__DATA,__mod_init_func")]
-#[cfg_attr(target_os = "windows", link_section = ".CRT$XCU")]
-pub static CTOR_ENTRY: extern "C" fn() = {
-    extern "C" fn ctor_thunk() {
-        ctor();
-    };
-    ctor_thunk
-};
 
 #[no_mangle]
 fn ctor() {
@@ -138,3 +60,14 @@ unsafe extern "C" fn init(env: sys::napi_env, exports: sys::napi_value) -> sys::
 
     s
 }
+
+#[used]
+#[cfg_attr(target_os = "linux", link_section = ".ctors")]
+#[cfg_attr(target_os = "macos", link_section = "__DATA,__mod_init_func")]
+#[cfg_attr(target_os = "windows", link_section = ".CRT$XCU")]
+pub static CTOR_ENTRY: extern "C" fn() = {
+    extern "C" fn ctor_thunk() {
+        ctor();
+    };
+    ctor_thunk
+};
